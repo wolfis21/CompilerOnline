47aa19d6d090419b59eae2d271bd4f218a9e358f
Isaac Saado <86305184+wolfis21@users.noreply.github.com>
2023-04-20 00:29:30 -0400

    Update models.py
    testing

--- CompilerOnline/system/models.py
+++ CompilerOnline/system/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+#prototype 1
 class Roles(models.Model):
     name_rol = models.CharField(max_length=200, verbose_name='nombre del Rol')
 
