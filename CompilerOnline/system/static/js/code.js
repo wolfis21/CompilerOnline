b6c259d22533d6f21f4d5fcd11e06aef32452d14
suchT01 <zorrillaja30@gmail.com>
2023-05-05 09:13:42 -0400

    bug de boton subir

--- CompilerOnline/system/static/js/code.js
+++ CompilerOnline/system/static/js/code.js
@@ -25,23 +25,3 @@ $(document).ready(function(){
       } // End if
     });
   });
-  
-/* mas informacion */
-
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 100){
-        document.querySelector('.container-boton-subir')
-        .classList.add('show')
-    }else{
-        document.querySelector('.container-boton-subir')
-        .classList.remove('show')
-    }
-};
-
-document.querySelector('.container-boton-subir')
-.addEventListener('click',()=>{
-    window.scrollTo({
-        top:0,
-        behavior:'smooth'
-    });
-});
